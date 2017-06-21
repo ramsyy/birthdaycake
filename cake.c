10c75c77ee59d29af6f9c88af91939cb4bfbcf52
ramsy <ramsyy@users.noreply.github.com>
2017-06-21 14:20:59 +0530

    Create cake.c

---
+++ cake.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+#include<string.h>
+
+main()
+{
+char * A = "\n\n\n\t\t\tFROM RESHU \n\t\t\t TO SUMEET :\n\n\t\t\tLoading............ 10%\n________________________________________________________________________________";
+char * B = "\n\n\n\t\tHAPPY BIRTHDAY TO YOU\n\n\t\t\tHAPPY BIRTHDAY TO YOU\n\n\t\t\t\tHAPPY BIRTHDAY TO YOU DEAR SUMEET\n\n\t\t\t\t\tHAPPY BIRTHDAY TO YOU\n";
+char * C = "\n\n\n\t\t             i i i i i\n\t\t             i i i i i\n\t\t           __i_i_i_i_i__\n\t\t          |#  #  #  #  #|\n\t\t        __| #  12/05  # |__\n\t\t       | #   #   #   #   # |\n\t\t     __|#   #HAPPY    #   #|__\n\t\t    |  #   #   BIRTHDAY#   #  |\n\t\t    | #   #             #   # |\n\t\t____|_________________________|____\n\n";
+char * D = "\n\n\n\tI hope you have a wonderful day and that the year ahead is filled with\n much love, many wonderful surprises and gives you lasting memories that you\n will cherish in all the days ahead.\n\n\t Happy Birthday SUMEET.";
+
+int i,j;
+char p='%';
+system("color A");
+for(i=0;i<strlen(A);i++)
+{
+    for(j=0;j<15000000;j++)
+    {}
+    printf("%c",A[i]);
+}
+    system("cls");
+    printf("\n\n\n\t\t\tFROM RESHU \n\t\t\t TO SUMEET :\n\n\t\t\tLoading............ 20%c \n________________________________________________________________________________",p);
+    printf("%c",A[i]);
+system("color D");
+for(i=0;i<strlen(B);i++)
+{
+    for(j=0;j<30000000;j++){}
+    printf("%c",B[i]);
+}
+
+    system("cls");
+    printf("\n\n\n\t\t\tFROM RESHU \n\t\t\t TO SUMEET :\n\n\t\t\tLoading............ 40%c \n________________________________________________________________________________",p);
+
+system("color F");
+for(i=0;i<strlen(C);i++)
+{
+    for(j=0;j<30000000;j++){}
+    printf("%c",C[i]);
+}
+   system("cls");
+   printf("\n\n\n\t\t\tFROM RESHU \n\t\t\t TO SUMEET :\n\n\t\t\tLoading............ 70%c \n________________________________________________________________________________",p);
+   printf("%c",C[i]);
+system("color E");
+for(i=0;i<strlen(D);i++)
+{
+    for(j=0;j<15000000;j++){}
+    printf("%c",D[i]);
+}
+system("cls");
+   printf("\n\n\n\t\t\tFROM RESHU \n\t\t\t TO SUMEET :\n\n\t\t\tLoading............ 95%c \n________________________________________________________________________________",p);
+
+system("cls");
+   printf("\n\n\n\t\t\tFROM RESHU \n\t\t\t TO SUMEET :\n\n\t\t\tLoading............ 100%c !!!\n________________________________________________________________________________\n\n",p);
+
+    system("color b");
+    system("cls");
+    printf("\n");
+    printf("\n         ___     ___     ___     ___     ___     ___     ___     ___       \n");
+    printf("     ___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\___   \n");
+    printf("    /   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\  \n");
+    printf("    \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/  \n");
+    printf("    /   \\___/                                                   \\___/   \\  \n");
+    printf("    \\___/     ######    ######   ############                       \\___/  \n");
+    printf("    /   \\     ######    ######   ##############                     /   \\  \n");
+    printf("    \\___/     ######    ######   ######    #####                    \\___/  \n");
+    printf("    /   \\     ######    ######   ######    #####     ###   ###      /   \\  \n");
+    printf("    \\___/     ################   ##############     ###########     \\___/  \n");
+    printf("    /   \\     ################   #############       #########      /   \\  \n");
+    printf("    \\___/     ################   ###############      #######       \\___/  \n");
+    printf("    /   \\     ################   ################       ###         /   \\  \n");
+    printf("    \\___/     ######    ######   ######    ######        #          \\___/  \n");
+    printf("    /   \\     ######    ######   ######    ######                   /   \\  \n");
+    printf("    \\___/     ######    ######   ###############     12/05/1996     \\___/  \n");
+    printf("    /   \\___  ######    ######   ##############                  ___/   \\  \n");
+    printf("    \\___/   \\___     ___     ___     ___     ___     ___     ___/   \\___/  \n");
+    printf("    /   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\  \n");
+    printf("    \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/  \n");
+    printf("        \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/   \\___/      \n");
+    printf("\n\n");
+do{
+system("color D");
+system("color E");
+system("color F");
+system("color A");
+
+
+for(j=0;j<50;j++){}}while(0!=1);
+system("cls");
+system("PAUSE");
+
+}
